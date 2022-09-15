9042d2a59651d1af4f986e187f552daf7e009b41
yyjpj <yyjpj@>
2022-09-15 20:48:35 +0800

    添加项目文件。

---
+++ MainForm.Designer.cs
@@ -0,0 +1,91 @@
+﻿namespace FindMyWiFiPassword
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listView_ExistWiFi = new System.Windows.Forms.ListView();
+            this.button_ListExisting = new System.Windows.Forms.Button();
+            this.button_ListPassword = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // listView_ExistWiFi
+            // 
+            this.listView_ExistWiFi.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.listView_ExistWiFi.HideSelection = false;
+            this.listView_ExistWiFi.Location = new System.Drawing.Point(26, 26);
+            this.listView_ExistWiFi.Name = "listView_ExistWiFi";
+            this.listView_ExistWiFi.Size = new System.Drawing.Size(296, 221);
+            this.listView_ExistWiFi.TabIndex = 0;
+            this.listView_ExistWiFi.UseCompatibleStateImageBehavior = false;
+            this.listView_ExistWiFi.View = System.Windows.Forms.View.List;
+            // 
+            // button_ListExisting
+            // 
+            this.button_ListExisting.Location = new System.Drawing.Point(359, 26);
+            this.button_ListExisting.Name = "button_ListExisting";
+            this.button_ListExisting.Size = new System.Drawing.Size(177, 70);
+            this.button_ListExisting.TabIndex = 1;
+            this.button_ListExisting.Text = "列出已连接过的WLAN";
+            this.button_ListExisting.UseVisualStyleBackColor = true;
+            this.button_ListExisting.Click += new System.EventHandler(this.button_ListExisting_Click);
+            // 
+            // button_ListPassword
+            // 
+            this.button_ListPassword.Enabled = false;
+            this.button_ListPassword.Location = new System.Drawing.Point(359, 177);
+            this.button_ListPassword.Name = "button_ListPassword";
+            this.button_ListPassword.Size = new System.Drawing.Size(177, 70);
+            this.button_ListPassword.TabIndex = 2;
+            this.button_ListPassword.Text = "列出选中WLAN的密码";
+            this.button_ListPassword.UseVisualStyleBackColor = true;
+            this.button_ListPassword.Click += new System.EventHandler(this.button_ListPassword_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(568, 275);
+            this.Controls.Add(this.button_ListPassword);
+            this.Controls.Add(this.button_ListExisting);
+            this.Controls.Add(this.listView_ExistWiFi);
+            this.DoubleBuffered = true;
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "查找已连接过的WLAN密码";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView listView_ExistWiFi;
+        private System.Windows.Forms.Button button_ListExisting;
+        private System.Windows.Forms.Button button_ListPassword;
+    }
+}
+
---
+++ MainForm.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Text.RegularExpressions;
+
+
+namespace FindMyWiFiPassword
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// 调用CMD并获取返回值
+        /// </summary>
+        /// <param name="command"></param>
+        /// <returns></returns>
+        public static string execCMD(string command)
+        {
+            System.Diagnostics.Process pro = new System.Diagnostics.Process();
+            pro.StartInfo.FileName = "cmd.exe";
+            pro.StartInfo.UseShellExecute = false;
+            pro.StartInfo.RedirectStandardError = true;
+            pro.StartInfo.RedirectStandardInput = true;
+            pro.StartInfo.RedirectStandardOutput = true;
+            pro.StartInfo.CreateNoWindow = true;
+            //pro.StartInfo.WindowStyle = System.Diagnostics.ProcessWindowStyle.Hidden;
+            pro.Start();
+            pro.StandardInput.WriteLine(command);
+            pro.StandardInput.WriteLine("exit");
+            pro.StandardInput.AutoFlush = true;
+            //获取cmd窗口的输出信息
+            string output = pro.StandardOutput.ReadToEnd();
+            pro.WaitForExit();//等待程序执行完退出进程
+            pro.Close();
+            return output;
+
+        }
+
+        private void button_ListExisting_Click(object sender, EventArgs e)
+        {
+            string originalText = execCMD("netsh wlan show profiles");
+            if(originalText.Contains("用户配置文件") == true)
+            {
+                string[] sArray = Regex.Split(originalText.Substring(originalText.IndexOf("用户配置文件")), Environment.NewLine);
+                if(sArray.Length >= 5)
+                {
+                    foreach(var i in sArray)
+                    {
+                        if(Regex.IsMatch(i, @"配置文件 : ."))
+                            listView_ExistWiFi.Items.Add(i.Substring(i.IndexOf(": ")).Replace(@": ",string.Empty));
+                    }
+                    if (listView_ExistWiFi.Items.Count > 0)
+                    {
+                        button_ListPassword.Enabled = true;
+                        MessageBox.Show("列出成功，请在左侧列表中选中要列出的WLAN后点击列出密码按钮", "Successful", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    }
+                    else
+                        MessageBox.Show("列出失败", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+                else
+                    MessageBox.Show("列出失败，原因可能有:\n此设备未连接过任何WLAN", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else
+            {
+                MessageBox.Show("列出失败，原因可能有:\n1.此设备未连接过任何WLAN\n2.软件没有权限调用CMD，请使用管理员权限运行\n3.账户权限不足，可能是公司或学校限制等","Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void button_ListPassword_Click(object sender, EventArgs e)
+        {
+            if(listView_ExistWiFi.SelectedItems.Count == 1)
+            {
+                string originalText = execCMD(string.Format(@"netsh wlan show profiles name=""{0}"" key=clear",listView_ExistWiFi.SelectedItems[0].Text));
+                if(originalText.Contains("关键内容") == true)
+                {
+                    string KeyText = originalText.Substring(originalText.IndexOf("关键内容"), originalText.IndexOf("费用设置") - originalText.IndexOf("关键内容"));
+                    MessageBox.Show(KeyText.Substring(KeyText.IndexOf(@": ")).Replace(@": ", string.Empty), listView_ExistWiFi.SelectedItems[0].Text + "的密码是：", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                else
+                {
+                    MessageBox.Show("未获取到WLAN信息或密码，下方将输出CMD命令原文\n" + originalText, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+    }
+}
